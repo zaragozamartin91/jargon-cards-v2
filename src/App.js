4b1412a2eca6c600695164ea1ed674e0967c3f7e
zaragozamartin91 <zaragozamartin91@gmail.com>
2023-10-07 21:22:25 +0200

    Adding browser router demo

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 
 import React, { Suspense, lazy } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const App = () => (
   <Router basename="/jargon-cards-v2" >
     <Suspense fallback={<div>Loading...</div>}>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </Suspense>
   </Router>
@@ -21,11 +24,23 @@ function Home() {
       <p>
         Edit <code>src/App.js</code> and save to reload.
       </p>
+      <Link to="about">About this page</Link>
       <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
         Learn React
       </a>
     </header>
   </div>;
+}
+
+function About(params) {
+  return <div className="App">
+  <header className="App-header">
+    <img src={logo} className="App-logo" alt="logo" />
+    <p>
+      This is the ABOUT section.
+    </p>
+  </header>
+</div>;
 }
 
 export default App;
