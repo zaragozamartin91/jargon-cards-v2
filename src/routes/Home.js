69c79cb3a43ad7bc7d91e0036bb052b98c7b50fd
zaragozamartin91 <zaragozamartin91@gmail.com>
2023-10-08 15:40:29 +0200

    Making paths absolute

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -9,10 +9,10 @@ function Home() {
     <header className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
       <p>This is the main page, now being deployed on a custom workflow</p>
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
-      <Link to="about">About this page</Link>
+      <p>Edit <code>src/App.js</code> and save to reload.</p>
+      
+      <Link to="/about">About this page</Link>
+
       <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
         Learn React
       </a>
